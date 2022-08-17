213fa1a70b15f82f9907b4eee20dccb0334144ec
uma kumari <uma kumari>
2022-08-17 11:05:12 +0530

    add chapter file

---
+++ lib/screens/managements/chapter_management.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ChapterManagement extends StatefulWidget {
+  ChapterManagement({Key? key}) : super(key: key);
+
+  @override
+  State<ChapterManagement> createState() => _ChapterManagementState();
+}
+
+class _ChapterManagementState extends State<ChapterManagement> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
